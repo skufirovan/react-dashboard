f67ad4ae7fd05f47ec12034e9b39d737da587e86
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-09-16 23:03:25 +0300

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,16 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
   resolve: {
     alias: {
       "@app": resolve(__dirname, "src/app"),
       "@pages": resolve(__dirname, "src/pages"),
+      "@shared": resolve(__dirname, "src/shared"),
       "@widgets": resolve(__dirname, "src/widgets"),
     },
   },
