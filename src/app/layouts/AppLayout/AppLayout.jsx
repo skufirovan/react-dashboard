832ac3bef6179e418ab71fe622052feaba19d969
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-09-16 23:43:23 +0300

    refactor: tweak app layout

--- src/app/layouts/AppLayout/AppLayout.jsx
+++ src/app/layouts/AppLayout/AppLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "@widgets/index";
+import { PageTitle } from "@shared/ui/index";
+import s from "./AppLayout.module.scss";
 
 const AppLayout = () => {
   return (
-    <div style={{ display: "flex" }}>
+    <div className={s.layout}>
       <Sidebar />
-      <main style={{ flex: 1, padding: "1rem" }}>
-        <Outlet />
+      <main className={s.main}>
+        <PageTitle />
+        <div className={s.page}>
+          <Outlet />
+        </div>
       </main>
     </div>
   );
